0257329768716e968a57c59f08b5523a4c7b581e
Niki <43398321+niki-cy@users.noreply.github.com>
2019-04-16 14:46:47 +0900

    Update desktop.js

--- src/js/desktop.js
+++ src/js/desktop.js
@@ -1,31 +1,27 @@
 (function(PLUGIN_ID) {
-    'use strict';
+  'use strict';
 
-    var config = kintone.plugin.app.getConfig(PLUGIN_ID); // Get plug-in setting configuration
-    var TIMESTAMP_FIELD = config.select_datetime_field; // Field code of Datetime field
-    var STATUS_NAME = config.select_status; // Name of status
+  var config = kintone.plugin.app.getConfig(PLUGIN_ID); // Get plug-in setting configuration
+  var TIMESTAMP_FIELD = config.select_datetime_field; // Field code of Datetime field
+  var STATUS_NAME = config.select_status; // Name of status
 
-    // Specify Status change events
-    var statusEvents = [
-        'app.record.detail.process.proceed',
-        'mobile.app.record.detail.process.proceed'
-    ];
-
+  // Specify Status change events
+  var statusEvents = [
+    'app.record.detail.process.proceed'
+  ];
     // When process management is changed do the following
-    kintone.events.on(statusEvents, function(event) {
-        var currenttime = new Date(); // Get current datetime
-        var timestamp = currenttime.toISOString(); // Change date object to ISO string
-        var record = event.record; 
-        var status = event.nextStatus.value;
-        var datetimeFieldSetting = record[TIMESTAMP_FIELD];
+  kintone.events.on(statusEvents, function(event) {
+    var currenttime = new Date(); // Get current datetime
+    var timestamp = currenttime.toISOString(); // Change date object to ISO string
+    var record = event.record;
+    var status = event.nextStatus.value;
+    var datetimeFieldSetting = record[TIMESTAMP_FIELD];
 
-        if (!TIMESTAMP_FIELD) {
-            alert('Status Timestamp Plug-in Warning:\nThe datetime field is not defined in the plug-in settings.');
-        } else if (status === STATUS_NAME) {
-            datetimeFieldSetting['value'] = timestamp;
-        };
-        
-        return event;
-    })
-
+    if (!TIMESTAMP_FIELD) {
+      alert('Status Timestamp Plug-in Warning:\nThe datetime field is not defined in the plug-in settings.');
+    } else if (status === STATUS_NAME) {
+      datetimeFieldSetting.value = timestamp;
+    }
+    return event;
+  });
 })(kintone.$PLUGIN_ID);
